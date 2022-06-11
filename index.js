9025ccc5a03d78395d56dd7d7d06f223b9fd36f9
attia-mahmoud <attimoovy@gmail.com>
2022-06-11 08:23:45 +0300

    add support for multiple digit values

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let displayValue = "";
+let value = "";
 let operands = [null, null];
 let operator = null;
 let prevResult = null;
@@ -57,33 +58,26 @@ function clear() {
 
 function calculate(operands, operator) {
   if (prevResult != null && operands[1] == null) {
-    prevResult = operate(
-      operator,
-      parseFloat(prevResult),
-      parseFloat(operands[0])
-    );
+    prevResult = operate(operator, +prevResult, +operands[0]);
   } else {
-    prevResult = operate(
-      operator,
-      parseFloat(operands[0]),
-      parseFloat(operands[1])
-    );
+    prevResult = operate(operator, +operands[0], +operands[1]);
   }
   clear();
   document.querySelector(".display").innerHTML = Math.round(prevResult);
+  value = "";
 }
 
 function setOperator(button) {
+  if (value != "") setNumber();
+  value = "";
   operator = button.innerHTML;
   if (operands[0] == null) {
     addPrevResult();
   }
 }
 
-function setNumbers(button) {
-  operands[0] == null
-    ? (operands[0] = button.innerHTML)
-    : (operands[1] = button.innerHTML);
+function setNumber() {
+  operands[0] == null ? (operands[0] = +value) : (operands[1] = +value);
 }
 
 document.querySelector(".clear").addEventListener("click", function () {
@@ -91,6 +85,7 @@ document.querySelector(".clear").addEventListener("click", function () {
 });
 
 document.querySelector(".equals").addEventListener("click", function () {
+  setNumber();
   calculate(operands, operator);
 });
 
@@ -102,7 +97,8 @@ characters.forEach((character) => {
 
 numbers.forEach((number) => {
   number.addEventListener("click", function () {
-    setNumbers(number);
+    // setNumber(number);
+    value += number.innerHTML;
   });
 });
 
